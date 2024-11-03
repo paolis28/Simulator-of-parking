17a285cfcc64e7d68a8332db6624802fa75081d5
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-11-03 15:55:28 -0600

    [ADD] Car directions

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module estacionamiento
 
 go 1.22.2
+
+require github.com/oakmound/oak/v4 v4.1.1
+
+require (
+	github.com/disintegration/gift v1.2.1 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/exp v0.0.0-20220414153411-bcd21879b8fd // indirect
+	golang.org/x/image v0.21.0 // indirect
+	golang.org/x/mobile v0.0.0-20220325161704-447654d348e3 // indirect
+)
--- models/Car.go
+++ models/Car.go
@@ -1,6 +1,51 @@
 package models
 
+import (
+	"sync"
+
+	"github.com/oakmound/oak/v4/alg/floatgeom"
+	"github.com/oakmound/oak/v4/entities"
+	"github.com/oakmound/oak/v4/render"
+	"github.com/oakmound/oak/v4/render/mod"
+	"github.com/oakmound/oak/v4/scene"
+)
+
 const (
 	EntranceSpotX = 355.00 // punto de entrada estacionamiento
 	Speed         = 10     // velocidad del auto
 )
+
+type Car struct {
+	mu     sync.Mutex
+	area   floatgeom.Rect2
+	entity *entities.Entity
+}
+
+func NewCar(ctx *scene.Context) *Car {
+	area := floatgeom.NewRect2(300, 490, 320, 510)
+
+	modelToImage := map[int]string{
+		1: "assets/img/brownCar.png",
+		2: "assets/img/greenCar.png",
+		3: "assets/img/orangeCar.png",
+		4: "assets/img/whiteCar.png",
+	}
+	carModelNum := int(getRandomNumber(1, 4))
+	carModel, _ := modelToImage[carModelNum]
+
+	sprite, _ := render.LoadSprite(carModel)
+
+	newSwitch := render.NewSwitch("up", map[string]render.Modifiable{
+		"up":    sprite,
+		"down":  sprite.Copy().Modify(mod.FlipY),
+		"left":  sprite.Copy().Modify(mod.Rotate(90)),
+		"right": sprite.Copy().Modify(mod.Rotate(-90)),
+	})
+
+	entity := entities.New(ctx, entities.WithRect(area), entities.WithRenderable(newSwitch), entities.WithDrawLayers([]int{1, 2}))
+
+	return &Car{
+		area:   area,
+		entity: entity,
+	}
+}
